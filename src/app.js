41932677b0154cc4ba31c2893c564f077aa18866
tomasbarreiror <tomasbarreiror@gmail.com>
2024-02-03 18:46:28 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,52 +1,51 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import handlebars from "express-handlebars";
 import IndexRouter from "./routes/index.routes.js";
 import { Server } from "socket.io";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const PORT = process.env.port
-const DB_URL = process.env.DB_URL
+const PORT = process.env.port;
+const DB_URL = process.env.DB_URL;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static("src/public"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("src/public"));
 
-app.engine("handlebars", handlebars.engine())
+app.engine("handlebars", handlebars.engine());
 app.set("views", "src/views");
 app.set("view engine", "handlebars");
 
 app.use((req, res, next) => {
-  req.io = io
-  next()
-})
+	req.io = io;
+	next();
+});
 
-app.use("/", IndexRouter)
+app.use("/", IndexRouter);
 
 const server = app.listen(PORT, (error) => {
+	if (error) {
+		console.log(error);
+	}
 
-  if (error) {
-    console.log(error)
-  }
+	console.log("servidor esta running en el puerto " + PORT);
+});
 
-  console.log("servidor esta running en el puerto " + PORT)
-})
-
-const io = new Server(server)
+const io = new Server(server);
 
 io.on("connection", (socket) => {
-	console.log("Se conecto un nuevo ususario")
-})
+	console.log("Se conecto un nuevo ususario");
+});
 
 startMongoConnection()
 	.then(() => {
-		console.log("Conectado a la base de datos")
+		console.log("Conectado a la base de datos");
 	})
-	.catch((err) => console.log(err))
+	.catch((err) => console.log(err));
 
 async function startMongoConnection() {
-	await mongoose.connect(DB_URL)
+	await mongoose.connect(DB_URL);
 }
